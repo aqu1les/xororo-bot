6daadf1f6042a0103b329013588b64fb10715af9
aqu1les <felipebarros.dev@gmail.com>
2019-11-15 01:40:30 -0300

    fixed new command

--- src/commands/cu.js
+++ src/commands/cu.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     run: async (client, message, args) => {
-        message.reply(` você tem ${Math.random() * 100} de chances de comer o cu de ${args}`)
+        message.reply(` você tem ${Math.round(Math.random() * 100)}% de chances de comer o cu de @${args}`)
     },
     get command() {
         return {
