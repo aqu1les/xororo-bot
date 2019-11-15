3434af80993a97a230405446a921fe41de62e196
aqu1les <felipebarros.dev@gmail.com>
2019-11-15 01:53:42 -0300

    new command

---
+++ src/commands/lanse.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    run: async (client, message, args) => {
+        return message.channel.send(`${message.author.username} lansou a braba`);
+    },
+    get command() {
+        return {
+            name: 'cu',
+            usage: 'cu'
+        }
+    }
+}
