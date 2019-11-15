f86bf3542fba59863d5d07e78597cdd42186c7d5
aqu1les <felipebarros.dev@gmail.com>
2019-11-15 02:54:17 -0300

    elo ok

--- src/commands/elo.js
+++ src/commands/elo.js
@@ -3,19 +3,23 @@ const axios = require("axios");
 const API_KEY = process.env.API_KEY;
 
 async function getElo(username) {
-    const response = await axios.get(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${API_KEY}`);
-    const userId = response.data.id;
-    const eloResponse = await axios.get(`https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${userId}?api_key=${API_KEY}`);
-    const resp = [];
-    eloResponse.data.forEach(queue => {
-        if (queue.queueType === "RANKED_SOLO_5X5") {
-            return resp.push(`Solo: ${queue.tier} ${queue.rank}`);
-        }
-        if (queue.queueType === "RANKED_FLEX_SR") {
-            return resp.push(`Flex: ${queue.tier} ${queue.rank}`);
-        }
-    });
-    return resp;
+    try {
+        const response = await axios.get(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${API_KEY}`);
+        const userId = response.data.id;
+        const eloResponse = await axios.get(`https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${userId}?api_key=${API_KEY}`);
+        let resp = [];
+        eloResponse.data.forEach(queue => {
+            if (queue.queueType === "RANKED_SOLO_5X5") {
+                return resp.push(`Solo: ${queue.tier} ${queue.rank}`);
+            }
+            if (queue.queueType === "RANKED_FLEX_SR") {
+                return resp.push(`Flex: ${queue.tier} ${queue.rank}`);
+            }
+        });
+        return resp.join(" ");
+    } catch (err) {
+        return "Erro na conexão com a API da Rito Gomes!";
+    }
 }
 
 module.exports = {
