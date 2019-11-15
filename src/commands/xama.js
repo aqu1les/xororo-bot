30bffbd7b53ca15f273448055d6cefe5760126f1
aqu1les <felipebarros.dev@gmail.com>
2019-11-15 01:08:37 -0300

    xesques fixed

--- src/commands/xama.js
+++ src/commands/xama.js
@@ -1,31 +1,36 @@
 const Discord = require('discord.js');
 const db = require('../db/dao');
+const mongoose = require('mongoose');
 
-async function inc_xesque(xesques, id) {
-    await db.run(`UPDATE users SET XESQUES=${xesques+1} WHERE ID=${id};`, err => {
-        if(err) console.log(err);
-    });
-}
-async function create_user(ID, NAME) {
-    db.run(`INSERT INTO users(ID, NAME, XESQUES) VALUES('${ID}', '${NAME}', '1')`, err => {
-        if(err) console.log(err);
-    });
+const UserSchema = new mongoose.Schema({
+    uid: String,
+    name: String,
+    xesques: Number
+});
+
+const User = mongoose.model('user', UserSchema);
+
+async function inc_xesque(id, username) {
+    const user = await User.find({ uid: id });
+    if (user) {
+        user.set({ xesques: user.xesques + 1 });
+        user.save()
+            .then(u => {
+                return u.xesques;
+            })
+            .catch(err => console.log(err));
+    } else {
+        User.create({ uid: id, name: username, xesques: 1 })
+            .then(u => {
+                return u.xesques;
+            })
+            .catch(err => console.log(err));
+    }
 }
 module.exports = {
     run: (client, message) => {
-        message.channel.send('no xesquedele');
-             
-        db.all(`SELECT * FROM users WHERE ID=${message.author.id} LIMIT 1 ;`, async (err, user) => {
-            if(err) return console.log(err);
-            if(user.length > 0) {
-                message.channel.send(`Você xamou no xesque ${user[0].XESQUES+1} vezes !`);
-                inc_xesque(user[0].XESQUES, user[0].ID);
-            } else {
-                message.reply(`Você xamou no xesque pela primeira vez !`);
-                create_user(message.author.id, message.author.username);
-            }
-        });
-        return;
+        const xesques = inc_xesque(message.author.id, message.author.username);
+        return message.channel.send(`Você xamou no xesque ${xesques} vezes !`);
     },
     get command() {
         return {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs-extra');
 const Enmap = require('enmap');
+const mongoose = require('mongoose');
 
 const client = new Discord.Client({ forceFetchUser: true });
 
@@ -12,6 +13,12 @@ client.commands = new Enmap();
 
 const init = async () => {
     const cmds = await fs.readdir('src/commands');
+
+    try {
+        mongoose.connect(`mongodb+srv://aqu1les:${process.env.password}>@cluster0-kvfg5.mongodb.net/xororo?retryWrites=true&w=majority`, { useNewUrlParser: true })
+    } catch (e) {
+        console.log(e);
+    }
 
     cmds.map(f => {
         try {
