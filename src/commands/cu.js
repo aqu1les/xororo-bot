caafd61b2f77426ec3ff0692562c68dcbce15810
aqu1les <felipebarros.dev@gmail.com>
2019-11-15 03:42:16 -0300

    added validations to command

--- src/commands/cu.js
+++ src/commands/cu.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 
 module.exports = {
     run: async (client, message, args) => {
+        if (Array(args).length == 0) return message.reply(" marque a pessoa que você gostaria de comer o cu.");
+        if (String(args[0]).substring(0, 2) !== "<@") return message.reply(" por favor marque a pessoa que você gostaria de comer o cu.");
         const chances = Math.round(Math.random() * 100);
         if (chances === 100) {
             return message.reply(` com ${chances}% de chances, você comeu o cu de ${args}. Parabéns!`);
