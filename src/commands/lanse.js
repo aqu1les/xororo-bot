784ce60a5834dae25353a742d332be0e79db5da4
aqu1les <felipebarros.dev@gmail.com>
2019-11-17 00:05:41 -0300

    contador de brabas

--- src/commands/lanse.js
+++ src/commands/lanse.js
@@ -1,8 +1,31 @@
 const Discord = require('discord.js');
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    uid: String,
+    name: String,
+    xesques: Number,
+    brabas: Number
+});
+const User = mongoose.model('user', UserSchema);
+
+async function inc_brabas(id, username) {
+    const user = await User.findOne({ uid: id });
+    if (user) {
+        user.brabas = user.brabas + 1;
+        await user.save();
+        return user.brabas;
+    } else {
+        await User.create({ uid: id, name: username, brabas: 1 });
+        return 1;
+    }
+}
 
 module.exports = {
     run: async (client, message, args) => {
-        return message.channel.send(`${message.author.username} lansou a braba`);
+        const brabas = await inc_brabas();
+        if (brabas === 1) return message.reply(`lansou a braba pela primeira vez!`);
+        return message.channel.send(`Você lansou a braba ${brabas} vezes, fdp`);
     },
     get command() {
         return {
