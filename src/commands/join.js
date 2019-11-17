ab02c635c49617af2c4984414e1e2b91c5ce3501
aqu1les <felipebarros.dev@gmail.com>
2019-11-17 00:27:45 -0300

    test

--- src/commands/join.js
+++ src/commands/join.js
@@ -1,14 +1,13 @@
 const Discord = require('discord.js');
-const ytdl = require('ytdl-core');
 
 module.exports = {
     run: (client, message) => {
-        if(!message.guild.voiceConnection){
+        if (!message.guild.voiceConnection) {
             message.member.voiceChannel.join().then(connection => {
-                message.channel.send(`Joined the VoiceChannel "${connection.channel.name}" !`);
+                message.channel.send(`Entrei fodase !`);
             });
         } else {
-            if(message.guild.voiceConnection.channel.name == message.member.voiceChannel.name ) {
+            if (message.guild.voiceConnection.channel.name == message.member.voiceChannel.name) {
                 return message.channel.send(`Already connected to your channel !`);
             } else {
                 message.member.voiceChannel.join().then(connection => {
