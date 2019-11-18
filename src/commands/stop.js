19be4fc2bf2b3366bc45cfc939dafb35d752e76a
Felipe Barros <felipebarros.dev@gmail.com>
2019-11-18 16:40:13 -0300

    Update stop.js

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -4,8 +4,11 @@ module.exports = {
     run: (client, message) => {
         if (message.guild.voiceConnection) {
             if (message.guild.voiceConnection.dispatcher) {
+                message.channel.send("blz, fui de beise");
                return message.guild.voiceConnection.disconnect();
             }
+        } else {
+            return message.channel.send("tem nada pra parar porra");
         }
     },
     get command() {
