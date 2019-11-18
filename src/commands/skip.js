c4fdb42156216b12d81800747052ce10124a224b
Felipe Barros <felipebarros.dev@gmail.com>
2019-11-18 16:57:44 -0300

    Update skip.js

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -3,9 +3,10 @@ const Discord = require('discord.js');
 module.exports = {
     run: (client, message) => {
         if (message.guild.voiceConnection) {
-            if (message.guild.voiceConnection.dispatcher) message.guild.voiceConnection.player.dispatcher.end();
+            if (message.guild.voiceConnection.dispatcher) return message.guild.voiceConnection.player.dispatcher.end();
+        } else {
+            return message.channel.send("como que skipa se n ta tocando nada desgraça");
         }
-        return;
     },
     get command() {
         return {
