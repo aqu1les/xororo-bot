310f1a2a4666be5f1e61eb02db5e64d38d4576bd
Felipe Barros <felipebarros.dev@gmail.com>
2019-11-18 16:12:54 -0300

    Update stop.js

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -3,8 +3,7 @@ const Discord = require('discord.js');
 module.exports = {
     run: (client, message) => {
         if(message.guild.voiceConnection) {
-            if(message.guild.voiceConnection.dispatcher) message.guild.voiceConnection.player.dispatcher.end();
-            else {
+            if(message.guild.voiceConnection.dispatcher) {
                 message.guild.voiceConnection.dispatcher.end();
                 message.guild.voiceConnection.disconnect();
             }
