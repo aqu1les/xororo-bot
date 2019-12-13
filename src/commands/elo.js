7732b0d6cb9862d6be6e1653984f6c83c79fd5ad
aqu1les <felipebarros.dev@gmail.com>
2019-12-13 02:29:38 -0300

    RIOT API error

--- src/commands/elo.js
+++ src/commands/elo.js
@@ -5,8 +5,10 @@ const API_KEY = process.env.API_KEY;
 async function getElo(username) {
     try {
         const response = await axios.get(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${API_KEY}`);
+        if (!response.data) return "Usuario não existe!";
         const userId = response.data.id;
         const eloResponse = await axios.get(`https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${userId}?api_key=${API_KEY}`);
+        console.log(response.data);
         let resp = [];
         await eloResponse.data.map(queue => {
             if (queue.queueType == "RANKED_SOLO_5x5") {
@@ -18,6 +20,7 @@ async function getElo(username) {
         });
         return resp.join("\n");
     } catch (err) {
+        if (err.toJSON().message === "Request failed with status code 404") return "Usuario não existe!";
         return "Erro na conexão com a API da Rito Gomes!";
     }
 }
