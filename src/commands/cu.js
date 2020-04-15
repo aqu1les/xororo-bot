2a72d36d6e99920f6991c26931db03a268864cbc
Felipe Barros <felipebarros.dev@gmail.com>
2020-04-15 12:07:10 -0300

    bug fix

--- src/commands/cu.js
+++ src/commands/cu.js
@@ -15,8 +15,7 @@ module.exports = {
     run: async (client, message, args) => {
         if (Array(args).length == 0) return message.reply(" marque a pessoa que você gostaria de comer o cu.");
         if (String(args[0]).substring(0, 2) !== "<@") return message.reply(" por favor marque a pessoa que você gostaria de comer o cu.");
-        // const chances = Math.round(Math.random() * 100);
-        const chances = 100;
+        const chances = Math.round(Math.random() * 100);
         if (chances === 100) {
             inc_cus(message.author.id, message.author.username, String(args[0]));
             return message.reply(` com ${chances}% de chances você comeu o cu de ${args}. Parabéns!`);
